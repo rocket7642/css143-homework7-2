f77a7ba3ca1ed1e5000409f1380fc1446b1abf82
rocket7642 <49769828+rocket7642@users.noreply.github.com>
2020-03-13 21:18:10 -0700

    all work
    now to work on performance

--- src/MaxHeapTest.java
+++ src/MaxHeapTest.java
@@ -1,5 +1,80 @@
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class MaxHeapTest {
-    // homework
+    @Test
+    public void MaxLogND(){
+        Integer[] input = {1,3,8,4,5,9};
+        Integer[] result = {9,8,5,4,3,1};
+        MaxHeap test = new MaxHeap(63);
+        test.MaxHeapLogN(input);
+        test.display();
+        
+        for(int i = 0; i < result.length; i++){
+            assertEquals(result[i],test.data[i]);
+        }
+        
+    }
+    
+    @Test
+    public void MaxND(){
+        Integer[] input = {1,3,8,4,5,9};
+        Integer[] result = {9,5,8,4,3,1};
+        
+        MaxHeap test = new MaxHeap(63);
+        test.MaxHeapN(input);
+        test.display();
+        
+        for(int i = 0; i < result.length; i++){
+            assertEquals(result[i],test.data[i]);
+        }
+    }
+    
+    @Test
+    public void addD(){
+        Integer[] input = {1,3,8,4,5,9};
+        Integer[] preadd = {9,8,5,4,3,1};
+        Integer[] result = {9,8,6,4,3,1,5};
+        
+        MaxHeap test = new MaxHeap(63);
+        test.MaxHeapLogN(input);
+        
+        for(int i = 0; i < preadd.length; i++){
+            assertEquals(preadd[i],test.data[i]);
+        }
+        
+        test.add(6);
+        
+        for(int i = 0; i < result.length; i++){
+            assertEquals(result[i],test.data[i]);
+        }
+    }
+    
+    @Test
+    public void getD(){
+        Integer[] input = {1,3,8,4,5,9};
+        Integer result = 9;
+        
+        MaxHeap test = new MaxHeap(63);
+        test.MaxHeapLogN(input);
+        
+        assertEquals(result,test.get());
+    }
+    
+    @Test
+    public void popD(){
+        Integer[] input = {1,3,8,4,5,9};
+        Integer[] result = {8,4,5,1,3};
+        Integer output = 9;
+        
+        MaxHeap test = new MaxHeap(63);
+        test.MaxHeapLogN(input);
+        
+        assertEquals(output,test.pop());
+        test.display();
+        
+        for(int i = 0; i < result.length; i++){
+            assertEquals(result[i],test.data[i]);
+        }
+    }
 }
